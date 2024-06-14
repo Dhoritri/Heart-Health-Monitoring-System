2b60c75bb95b6a57ce8d9a089e7dadbde014d1ca
Dhoritri <ishrak.alam32@gmail.com>
2024-06-15 01:50:33 +0600

    doctors update

--- js/style.js
+++ js/style.js
@@ -9,4 +9,31 @@ window.onscroll=()=>{
     menu.classList.remove('fa-times')
     nav.classList.remove('active')
 }
- 
+
+$(document).ready(function(){
+    $('.doctors-layout').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: true,
+        arrows: true,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
