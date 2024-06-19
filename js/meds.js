880a6295b1ddddc4a9ee4e4047447247381434be
Dhoritri <ishrak.alam32@gmail.com>
2024-06-20 02:18:23 +0600

    medicine and recommendation updated

--- js/meds.js
+++ js/meds.js
@@ -10,6 +10,10 @@ const recommendations = {
     endocarditis: ['Antibiotics', 'Antifungal medications (if fungal)', 'Surgery (in severe cases)'],
     pulmonaryHypertension: ['Endothelin receptor antagonists', 'Phosphodiesterase-5 inhibitors', 'Prostacyclin analogs', 'Calcium channel blockers']
 };
+document.addEventListener('DOMContentLoaded', function() {
+    alert("Please make sure to consult a physician before starting any medication. We are not responsible for any side effects.");
+});
+
 
 function showRecommendations() {
     const diseaseSelect = document.getElementById('disease');
