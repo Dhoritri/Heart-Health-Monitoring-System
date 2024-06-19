3a8bf33743dbdf29cc06d19460c01d957cb53847
Dhoritri <ishrak.alam32@gmail.com>
2024-06-19 18:09:06 +0600

    medicine and recommendations

---
+++ js/meds.js
@@ -0,0 +1,32 @@
+const recommendations = {
+    hypertension: ['ACE inhibitors', 'Beta-blockers', 'Calcium channel blockers', 'Diuretics'],
+    cad: ['Statins', 'Aspirin', 'Beta-blockers', 'Nitroglycerin'],
+    heartFailure: ['ACE inhibitors', 'Beta-blockers', 'Diuretics', 'Aldosterone antagonists'],
+    arrhythmia: ['Antiarrhythmic drugs', 'Beta-blockers', 'Calcium channel blockers', 'Anticoagulants'],
+    valvular: ['Diuretics', 'Anticoagulants', 'Beta-blockers', 'ACE inhibitors'],
+    congenital: ['Diuretics', 'Beta-blockers', 'ACE inhibitors', 'Prostaglandins'],
+    cardiomyopathy: ['Beta-blockers', 'ACE inhibitors', 'Diuretics', 'Anticoagulants'],
+    pericarditis: ['NSAIDs', 'Corticosteroids', 'Colchicine', 'Antibiotics (if bacterial)'],
+    endocarditis: ['Antibiotics', 'Antifungal medications (if fungal)', 'Surgery (in severe cases)'],
+    pulmonaryHypertension: ['Endothelin receptor antagonists', 'Phosphodiesterase-5 inhibitors', 'Prostacyclin analogs', 'Calcium channel blockers']
+};
+
+function showRecommendations() {
+    const diseaseSelect = document.getElementById('disease');
+    const selectedDisease = diseaseSelect.value;
+    const recommendationsSection = document.getElementById('recommendations');
+
+    recommendationsSection.innerHTML = '';
+
+    if (selectedDisease) {
+        const meds = recommendations[selectedDisease];
+        const ul = document.createElement('ul');
+        meds.forEach(med => {
+            const li = document.createElement('li');
+            li.textContent = med;
+            ul.appendChild(li);
+        });
+
+        recommendationsSection.appendChild(ul);
+    }
+}
